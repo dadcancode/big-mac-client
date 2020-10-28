cf91026774981dac1797a5f838b6bbd40b6eca4e
Zach <zachery.goodrich@gmail.com>
2020-10-28 12:21:40 -0700

    local results colors changed

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,7 +9,7 @@ const Form = (props) => {
                 {/* <label htmlFor='dollarAmt'>Enter dollar amount:</label> */}
                 <input className='form-control' type='number' id='dollarAmt' placeholder='Enter Dollar Amount' onChange={(e) => {props.setDollarAmt(e.target.value)}}/>
             </div>            
-            <button className='btn btn-outline-warning w-100' type='submit'>Enter</button>
+            <button className='btn btn-outline-light w-100' type='submit'>Enter</button>
         </form>
     )
 }
--- src/components/LocalResults/LocalResults.js
+++ src/components/LocalResults/LocalResults.js
@@ -6,16 +6,16 @@ const LocalResults = (props) => {
 
     
     return (
-        <div className='row bg-warning text-danger data-row'>
+        <div className='row bg-info text-dark data-row'>
             <div className='col-12 h-25 d-flex align-items-center'>
                 <h4>Local Results</h4>
             </div>
             {
                 props.randCountry.Country ?
                     <div className='col-12 d-flex h-75 justify-content-center'>
-                        <div className='row w-100 justify-content-around align-self-center h-100'>
-                            <div className='col-12 d-flex justify-content-center align-items-center'>You can buy <span>{`${calculateMacs(props.dollarAmt, props.macData.Localprice)}`}</span> Big Mac(s) in your country</div>
-                            <div className='col-12 d-flex justify-content-center align-items-center'>Your Dollar Purchase Parity (PPP) is: <span>{`${props.ppp}`}</span></div>
+                        <div className='row w-100 justify-content-around h-100'>
+                            <div className='col-5 d-flex flex-column align-items-center justify-content-between'>You can buy <span>{`${calculateMacs(props.dollarAmt, props.macData.Localprice)}`}</span> Big Mac(s) in your country</div>
+                            <div className='col-5 d-flex justify-content-center align-items-center'>Your Dollar Purchase Parity (PPP) is: <span>{`${props.ppp}`}</span></div>
                         </div>
                     </div>
                     : null
