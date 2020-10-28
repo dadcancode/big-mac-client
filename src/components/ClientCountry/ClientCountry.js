8ac0cad035fa935ed328bb86c07a9154f30b57ff
Zach <zachery.goodrich@gmail.com>
2020-10-28 11:52:35 -0700

    changed background of client country

--- src/components/ClientCountry/ClientCountry.js
+++ src/components/ClientCountry/ClientCountry.js
@@ -6,9 +6,9 @@ const ClientCountry = (props) => {
     
 
     return (
-        <div className='row bg-danger text-warning data-row'>
+        <div className='row bg-dark text-light data-row'>
             <div className='col-12 h-25 d-flex align-items-center'>
-                <h4>You live in: {props.ipLocation}</h4>
+                <h4>Your Country: {props.ipLocation}</h4>
             </div>
             <div className='col-12 h-75 d-flex justify-content-center align-items-center'>
                 <Form dollarAmt={props.dollarAmt} setDollarAmt={props.setDollarAmt} handleSubmit={props.handleSubmit} />
